3aad993e59f1caa2f313b6f097b98d0191fa6156
AbyLuna <abigaly33@gmail.com>
2021-05-14 11:12:22 -0500

    puse vista de menu y enlace, coloque  comando composer require barryvdh/laravel-dompdf   y modifique en config>app.php PROVIDERS y ALIASES

--- routes/web.php
+++ routes/web.php
@@ -12,17 +12,29 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/', function(){
+    return view('auth.login');
 });
+
+
 
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+/*Route::get('/', function(){
+    return view('menu');
+});*/
+
 
 Route::resource('contratos',App\Http\controllers\ContratoController::class);
 
 Route::resource('expedientes',App\Http\controllers\ExpedienteController::class);
 
 Route::resource('cajas',App\Http\controllers\CajaController::class);
+
+
+
+/*Route::get('/', function(){
+    return redirect('url');
+});*/
