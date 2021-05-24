eebc4303e590c7ecb768f562a6b57ca16442f516
AbyLuna <abigaly33@gmail.com>
2021-05-24 11:58:50 -0500

    Migracion  y crud  registros,  ruta de menu en /home, boton imprimir a un si ruta real.

--- resources/views/menu.blade.php
+++ resources/views/menu.blade.php
@@ -1,13 +1,6 @@
 
-<!DOCTYPE html>
-<html lang="es">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
-    <title>Menu</title>
-</head>
+@extends('layouts.app')
+@section('content')
 <body>
     <div class="container">
         <nav class="navbar navbar-expand-lg navbar-dark bg-white fondo-nav sticky-top">
@@ -21,30 +14,30 @@
         <div class="card-group">
             <div class="card">
                
-                <a class="btn btn-outline-secondary" href="{{ route('cajas.index') }}">{{ __('Cajas') }}
-                    <img src="/img/agregar1.png" width="280" height="auto" />
+                <a class="btn btn-outline-secondary" href="{{ route('cajas.index') }}">
+                    <img src="/img/reportes.png" width="280" height="auto" />
                 </a>
                 <div class="card-body">
                     <center>
-                        <h5> Ingreso de usuarios</h5>
-                        <p>Registrto y actualizaciones de usuarios.</p>
+                        <h5>Vale de Prestamo </h5>
+                        <p>Modulo de registro e impresion de vale de prestamo.</p>
                     </center>
                 </div>
             </div>
             <div class="card">
-            <a class="btn btn-outline-secondary" href="{{ route('cajas.index') }}">{{ __('Cajas') }}
-                    <img src="/img/caja.png" width="280" height="auto" />
+            <a class="btn btn-outline-secondary" href="{{ route('cajas.index') }}">
+                    <img src="/img/caja.png" width="255" height="auto" />
                 </a>
                 <div class="card-body">
                     <center>
-                        <h5>Registro</h5>
+                        <h5>Registro de Cajas</h5>
                         <p class="card-text"> Llenado de formularios para el registro de cajas y tomos</p>
                     </center>
                 </div>
             </div>
             <div class="card">
-            <a class="nav-link" href="{{ route('expedientes.index') }}">{{ __('Expedientes') }}
-                    <img src="/img/caja.png" width="280" height="auto" />
+            <a class="btn btn-outline-secondary" href="{{ route('expedientes.index') }}">
+                    <img src="/img/rep.png" width="255" height="auto" />
                 </a>
                 <div class="card-body">
                     <center>
@@ -72,5 +65,4 @@
     </div>
     <script src="{{ asset('js/app.js') }}"></script>
 </body>
-
-</html>
+@endsection
