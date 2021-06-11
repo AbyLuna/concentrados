0cd075d4cc8522f2b24e7c9711e983624fae453a
Ajs <jajs.albert@gmail.com>
2021-06-11 14:19:45 -0500

    merge de prueba

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -42,7 +42,7 @@
                     <a class="nav-link" href="{{ route('contratos.index') }}">{{ __('Contratos') }}</a>
                     
                     <a class="nav-link" href="{{ route('registros.index') }}">{{ __('Registro') }}</a> -->
-                    <a class="nav-link" href="{{ route('locations.index') }}">{{ __('UbicacionF') }}</a> -->
+                        
                     </ul>
                   @endif 
 
