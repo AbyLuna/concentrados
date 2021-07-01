05722317c706260d8b05c908171efc3d93a5011b
AbyLuna <abigaly33@gmail.com>
2021-06-30 20:50:44 -0500

    Portada de caja cambios

--- resources/views/portada.blade.php
+++ resources/views/portada.blade.php
@@ -8,55 +8,135 @@
 <title>Portada de caja</title>
 </head>
 <body>
-<div class="text-center ">
+<div class="text-center container ">
         <img src="/img/cabecera.png" width="777" height="auto">
-        </div>
-   <div class=" text-center">
-        <h3><b> Secretaria de Infraestructura y Obras Publicas </b> </h3>
-        <select class="btn-lg btn btn-outline-light" >
-           <strong>
-            <option>RECIDENCIA GENERAL ZONA SUR</option>
-            <option>RECIDENCIA GENERAL ZONA NORTE</option>
-            <option>RECIDENCIA GENERAL ZONA ESTE</option>
-            <option>RECIDENCIA GENERAL ZONA OESTE</option>
-           </strong>
-        </select>
-        <br>
-        <select class="btn-lg btn btn-outline-light" >
-            <strong>
-             <option>(DGCCYCE)</option>
-             <option>(DGCCYCE)</option>
-             <option>(DGCCYCE)</option>
-             <option>(DGCCYCE)</option>
-            </strong>
+        <h2><b> Secretaria de Infraestructura y Obras Publicas </b> </h2>
+        <div class="container text-center">
+               <h1>  <b>309/78</b> </h1>
+                {{-- <table class="table table-striped">
+                    <thead>
+                        <tr>
+                            <th>Número de caja</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                         @foreach ($cajas as $cajas)
+                             <tr>
+                               <td>{{ $cajas->numCaja }}</td>
+                        @endforeach
+                    </tbody>
+                </table>  --}}
+                        
+         </div>
+         <select  class="btn-lg btn btn-outline-light" >
+              <strong>
+                 <option>(DGCCYCE)</option>
+                 <option>(DGCCYCE)</option>
+                  <option>(DGCCYCE)</option>
+                  <option>(DGCCYCE)</option>
+              </strong><br>
          </select>
-            <h5> DOCUMENTACION DEL EJERCICIO: <br>
+            <h4> DOCUMENTACION DEL EJERCICIO: <br>
             <input class="btn-lg btn btn-outline-ligth" type="text"  placeholder="2015">
-            </h5>
-        </div>
-    <div class="container">
-        <div>
-            <h4>Número de caja:<span> <b>309/78</b> </span></h4>
-            {{-- <table class="table table-striped">
-                <thead>
-                    <tr>
-                        <th>Número de caja</th>
-                    </tr>
-                </thead>
-                <tbody>
-                     @foreach ($cajas as $cajas)
-                         <tr>
-                           <td>{{ $cajas->numCaja }}</td>
-                    @endforeach
-                </tbody>
-            </table>  --}}
-        </div>
-       
-        <br><br>
-        <div class=" text-center ">
-            <h4> <b> Descripción General del contenido </b></h4>
-        </div>
-        <table class="table table-condensed">
+            </h4>
+         <div class=" text-center "><br><br>
+            <h3> <b> Descripción General del contenido </b></h3>
+               <hr style="border-color:grey;">
+                    <select class="btn-lg btn btn-outline-light" style="color:rgb(0, 0, 0)">
+                        {{-- name="seleccion" size="1" style="width:700px" --}}
+                               <option >SECRETARIO PARTICULAR</option>
+                               <option> DIRECCIÓN GENERAL JURÍDICA</option>
+                               <option>SUBDIRECCIÓN JURÍDICA DE LO CONTENCIOSO</option>
+                               <option>OFICINA DE ASUNTOS CONTENCIOSOS Y PENALES</option>
+                               <option>OFICINA DE ASUNTOS LABORALES Y PROCESOS ADTVOS</option>
+                               <option>SUBDIRECCIÓN JURÍDICA DE CONTRATOS Y RESCISIONES</option>
+                               <option>OFICINA DE CONTRATOS </option>
+                               <option>OFICINA DE RESCISIONES</option>
+                               <option> UNIDAD ADMINISTRATIVA</option>
+                               <option>DEPARTAMENTO DE RECURSOS HUMANOS</option>
+                               <option> OFICINA DE NÓMINA</option>
+                               <option> OFICINA DE ADMINISTRACIÓN DE PERSONAL</option>
+                               <option> OFICINA DE DESARROLLO ORGANIZACIONAL</option>
+                               <option> DEPARTAMENTO DE RECURSOS FINANCIEROS</option>
+                               <option> OFICINA DE CONTROL DE EGRESOS</option>
+                               <option> OFICINA DE CONTABILIDAD</option>
+                               <option> OFICINA DE PROGRAMACIÓN Y PRESUPUESTACIÓN</option>
+                               <option> OFICINA DE CONTROL FINANCIERO DE OBRA</option>
+                              <option> DEPARTAMENTO DE RECURSOS MATERIALES Y SERVICIOS GENERALES</option>
+                              <option> OFICINA DE ADQUISICIONES</option>
+                              <option> OFICINA DE CONTROL VEHÍCULAR</option>
+                              <option> OFICINA DE CONTROL DE INVENTARIOS</option>
+                              <option> OFICINA DE SERVICIOS GENERALES</option>
+                              <option> DEPARTAMENTO DE TECNOLOGÍAS DE LA INFORMACIÓN</option>
+                              <option> OFICINA DE GOBIERNO ELECTRÓNICO Y DESARROLLO DE APLICACIONES</option>
+                              <option> OFICINA DE INFRAESTRUCTURA Y SOPORTE TÉCNICO</option>
+                              <option> DEPARTAMENTO DE ARCHIVO GENERAL</option>
+                              <option>  UNIDAD DE LICITACIONES</option>
+                              <option> OFICINA DE ANÁLISIS TÉCNICO ECONÓMICO</option>
+                              <option>  OFICINA DE LICITACIONES</option>
+                              <option> OFICINA DE ESTUDIOS DE MERCADO</option>
+                              <option>  UNIDAD DE TRANSPARENCIA</option>
+                              <option>  UNIDAD DE GÉNERO</option>
+                              <option>  SUBSECRETARÍA DE INFRAESTRUCTURA</option>
+                              <option>DIRECCIÓN GENERAL DE PROYECTOS, PROGRAMACIÓN Y PRESUPUESTO DE CAMINOS Y CARRETERAS ESTATALES</option>
+                              <option> SUBDIRECCIÓN DE PLANEACIÓN, PROGRAMACIÓN Y PRESUPUESTACIÓN DE OBRA </option>
+                              <option>   DEPARTAMENTO DE PLANEACIÓN DE OBRA</option>
+                              <option>   DEPARTAMENTO DE PROGRAMACIÓN Y PRESUPUESTACIÓN DE OBRA</option>
+                              <option> DEPARTAMENTO DE SEGUIMIENTO Y EVALUACIÓN DE OBRA</option>
+                              <option>  SUBDIRECCIÓN DE PRESUPUESTOS DE CARRETERAS Y CAMINOS ESTATALES</option>
+                              <option>  DEPARTAMENTO DE CONTROL Y REGISTRO DE INFORMACIÓN</option>
+                              <option> DEPARTAMENTO DE LIBROS BLANCOS Y AUDITORÍAS</option>
+                              <option> DIR. GRAL. DE CONSTRUCCIÓN DE CAMINOS Y CARRETERAS ESTATALES</option>
+                              <option>  SUBDIRECCIÓN DE CONSTRUCCIÓN DE CAMINOS Y CARRETERAS ESTATALES</option>
+                              <option>  RESIDENCIA GENERAL DE CONSTRUCCIÓN DE CARRETERAS ZONA NORTE</option>
+                              <option>  RESIDENCIA GENERAL DE CONSTRUCCIÓN DE CARRETERAS ZONA CENTRO</option>
+                              <option> RESIDENCIA GENERAL DE CONSTRUCCIÓN DE CARRETERAS ZONA SUR</option>
+                              <option> SUBDIRECCIÓN DE CONTROL, SEGUIMIENTO DE CONST. DE CAMINOS Y CTRA.</option>
+                              <option>  DEPARTAMENTO DE SEGUIMIENTO OPERATIVO</option>
+                              <option>  DEPARTAMENTO DE CONTROL DE OBRA</option>
+                              <option> DEPARTAMENTO DE ESTIMACIONES</option>
+                              <option> SUBDIRECCIÓN DE MAQUINARIA</option>
+                              <option> DEPARTAMENTO TÉCNICO DE MAQUINARIA</option>
+                              <option> DEPARTAMENTO DE CONTROL DE MAQUINARIA</option>
+                              <option>  DEPARTAMENTO DE MANTENIMIENTO DE MAQUINARIA</option>
+                              <option> SUBSECRETARIA DE OBRAS PÚBLICAS Y COMUNICACIONES</option>
+                              <option>  DIRECCIÓN GENERAL DE PROYECTOS, PROGRAMACIÓN Y PPTO DE OBRAS PÚBLICAS</option>
+                              <option> SUBDIRECCIÓN DE PREINVERSIÓN Y EVALUACIÓN DE OBRA PÚBLICA</option>
+                              <option> DEPARTAMENTO DE INTEGRACIÓN DE PROYECTOS Y OBRAS PÚBLICAS</option>
+                              <option> DEPARTAMENTO DE INTEGRACIÓN DE PROYECTOS DE EDIFICACIÓN</option>
+                              <option>  DEPTO. DE CONTROL DE EVALUACIÓN DE PROGRAMAS OPERATIVOS DE INVERSIÓN</option>
+                              <option>  SUBDIRECCIÓN DE PRESUPUESTOS DE OBRAS PÚBLICAS</option>
+                              <option> DEPARTAMENTO DE COSTOS DE EDIFICACIÓN</option>
+                              <option>  DEPARTAMENTO DE SEGUIMIENTO Y CONTROL PRESUPUESTAL</option>
+                              <option>  DIRECCIÓN GENERAL DE CONSTRUCCIÓN DE OBRAS PÚBLICAS</option>
+                              <option>  SUBDIRECCIÓN DE CONSTRUCCIÓN DE OBRAS PÚBLICAS</option>
+                              <option>  DEPARTAMENTO DE CONSTRUCCIÓN ZONA NORTE</option>
+                              <option>  DEPARTAMENTO DE CONSTRUCCIÓN ZONA CENTRO</option>
+                              <option> DEPARTAMENTO DE CONSTRUCCIÓN ZONA SUR</option>
+                              <option>  DEPARTAMENTO DE ESTIMACIONES</option>
+                              <option> SUBDIR. DE PROGRAMACIÓN, CONTROL Y EVALUACIÓN  TÉC. DE OBRAS PÚBLICAS</option>
+                              <option> DEPARTAMENTO DE SEGUIMIENTO OPERATIVO</option>
+                              <option> DEPARTAMENTO DE CONTROL DE OBRA</option>
+                              <option>  DEPARTAMENTO DE SUPERVISIÓN DE OBRA</option>
+                              <option>  DIRECCIÓN GENERAL DE TELECOMUNICACIONES Y DESARROLLO DE VÍAS DE COMUNICACIÓN</option>
+                              <option> SUBDIRECCIÓN DE TELECOMUNICACIONES</option>
+                              <option> DEPARTAMENTO DE PROGRAMACIÓN Y PROYECTOS</option>
+                              <option> DEPARTAMENTO DE ENLACES DE TELECOMUNICACIONES</option>
+                              <option> SUBDIRECCIÓN DE VÍAS DE COMUNICACIÓN</option>
+                              <option> DEPARTAMENTO DE DESARROLLO DE PUERTOS</option>
+                              <option>  DEPARTAMENTO DE DESARROLLO FERROVIARIO</option>
+                              <option> COORDINACIÓN DE PLANEACIÓN Y EVALUACIÓN TÉCNICA</option>
+                              <option> DEPARTAMENTO DE CONTROL DE CALIDAD Y EVALUACIÓN TÉCNICA</option>
+                              <option>  DEPARTAMENTO DE VENTANILLA ÚNICA</option>
+                              <option> DEPARTAMENTO DE PLANEACIÓN Y EVALUACIÓN DE PROGRAMAS</option>
+                              <option> OFICINA DE ANÁLISIS E INTEGRACIÓN DE INFORMACIÓN</option>
+                              <option>OFICINA DE SEGUIMIENTO DE PROGRAMAS ESTATALES Y FEDERALES</option>
+                              <option> COORDINACIÓN SECTORIAL FONDEN</option>
+                          <hr style="border-color:grey;"> 
+                      </select>     
+           </div>
+          
+          <table class="table table-condensed">
             <thead>
                 <tr>
                     <th>EXP CTO</th>
@@ -64,8 +144,7 @@
                     <th>Bitacora</th>
                     <th>Tomos</th>
                 </tr>
-            </thead>
-           
+            </thead> 
             <tbody>
                  @foreach ($contratos as $contratos)
                      <tr>
@@ -76,11 +155,12 @@
                   </tr>
                 @endforeach
             </tbody>
-        </table>
-
-        <div class="text-center">
             <hr style="border-color:grey;">
-            <h5><font color=808080><b> VIGENCIA DOCUMENTAL </b></h5></font>
+ 
+           </table>
+          <div class="text-center">
+            <hr style="border-color:grey;">
+            <h4><font color=black><b> VIGENCIA DOCUMENTAL </b></h4></font>
                 <input class="btn-md btn btn-outline-ligth" type="text"  placeholder="Baja septiembre 2021">
                 <br>
                 <select class="btn-sm btn btn-outline-light" >
@@ -89,13 +169,13 @@
                      <option>(PARA ELIMINACIÓN ) </option>
                     </strong>
                  </select>
-            <hr style="border-color:grey;">
-            <h6><font color=808080><b> ARCHIVO DE CONCENTRACION </b></h6></font>
-            <hr style="border-color:grey;">
+                 <hr style="border-color:grey;">
+                 <h6 style=" color:black;"s><b> ARCHIVO DE CONCENTRACION </b></h6></>
+                 <hr style="border-color:grey;">    
             <div class="text-center">
-        <h6> NOMBRE Y FIRMA DEL RESPONSABLE DEL ARCHIVO DE TRAMITE </h6>
-        <br><br>
-             <hr size=5 width="330" style="border-color:grey;">
+           <h5> NOMBRE Y FIRMA DEL RESPONSABLE DEL ARCHIVO DE TRAMITE </h5>
+           <br><br>
+             <hr size=5 width="333" style="border-color:grey;">
              <strong>
              <select class="btn-md btn btn-outline-light" >
                  <option>Clementina Gadir Bautista González</option>
@@ -139,7 +219,8 @@
              <br>
         </div>
     </div>
-        </div>
+ 
+</div>
 </body>
 <script src="{{ asset('js/app.js') }}" defer></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"
